77cabd1e5450f12b3314c6cfc89e5449be00d095
mtsonkova <mtsonkova950@gmail.com>
2024-12-28 19:38:49 +0200

    New project created

---
+++ pageObjects/BasePage.js
@@ -0,0 +1,28 @@
+import {expect} from '@playwright/test';
+
+export class BasePage{
+    constructor(page) {
+        this.page = page;
+    }
+
+    async click(locator, page = this.page) {
+        await page.click(locator);
+    }
+
+    async fill(locator, page = this.page, text) {
+        await page.fill(locator, text);
+    }
+
+    async check(locator, page = this.page) {
+        await page.getByRole(locator).check();
+    }
+
+    async selectOptionInDropdown(locator, page= this.page, value) {
+        let element = page.locator;
+        await element.selectOption(value);
+    }
+
+    async verifyText(locator, page=this.page, text) {
+        await expect(page.locator).toHaveText(text);
+    }
+}
